89d2a27a48612943031d90fa9d6bac73a9783c8b
Ranjan Verma <verma.ranjan62@gmail.com>
2026-08-11 15:13:58 +0530

    Main Component Design

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -3,16 +3,21 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <nav className='navbar container pt-3 pb-3'>
-      <a href="" className='navbar-brand text-light'>Stock Prediction Portal</a>
+      <nav className='navbar container pt-3 pb-3 align-items-start'>
+        <a href='' className='navbar-brand text-light'>
+          Stock Prediction Portal
+        </a>
 
-      <div>
-        <a className='btn btn-outline-info' href="">Login</a>
-        &nbsp;
-        <a className='btn btn-info' href="">Register</a>
-
-      </div>
-    </nav>
+        <div>
+          <a className='btn btn-outline-info' href=''>
+            Login
+          </a>
+          &nbsp;
+          <a className='btn btn-info' href=''>
+            Register
+          </a>
+        </div>
+      </nav>
     </>
   )
 }
